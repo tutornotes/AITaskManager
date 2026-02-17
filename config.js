3b885dabccde0667afd982736a878f1560e2c5ac
tutornotes <msccs202425@gmail.com>
2026-02-17 22:27:03 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Shared configuration
 window.APP_CONFIG = {
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwrkf-GCAdsNcd_a9JSn8wS_j1VH1jT7G5HtESGZzAzcUjVlASDujdo5j_XDC-rJrpm/exec',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbywUzcJvjV1_En95Fg9YadV8_9b7LGEzFK8H8Sx7kSncIwDlNjoNsMshjNRLJSf6VGf/exec',
   GOOGLE_CLIENT_ID: '117548045551-4gh98h2i28obgaeit1ptf6l339srnp0i.apps.googleusercontent.com'
 };
