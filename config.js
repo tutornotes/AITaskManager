3001bd00619ac9d1963146a7a1869fbd0932c3b3
tutornotes <msccs202425@gmail.com>
2026-03-13 17:59:12 +0530

    Change WEB_APP_URL in config.js
    Updated WEB_APP_URL in the configuration.

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // Shared configuration
 window.APP_CONFIG = {
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxZGx30BpMaLqrvH4FTNelfn0PveXdv9P-vHxWv_nQUYcXT8DBQl8kRtnZKwEsEmn4U/exec',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw70H4fytzCeIXcw7GD94u43iIZPYRXpEy3w093Ys-dEi37TUkIp80Hrivs1wB62Dxq/exec',
   GOOGLE_CLIENT_ID: '117548045551-4gh98h2i28obgaeit1ptf6l339srnp0i.apps.googleusercontent.com'
 
 };
+
