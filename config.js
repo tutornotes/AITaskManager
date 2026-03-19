2af29f58b8f3c5966f996f06caf34c689b4cde63
tutornotes <msccs202425@gmail.com>
2026-03-19 19:06:41 +0530

    web url update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Shared configuration
 window.APP_CONFIG = {
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw7s-rrDEaXgbvrNZUAq5FBn17fBJHg-vJjMdXcjDuDZEbWFs4R1vmqNiLQ-9bGRge2/exec',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxCfVZ7KrJ6gTEvEhI2cv-gndX-WM1O6a79I6__HuDZoo2ApLOnWnOm_b-t5ogVPGFf/exec',
   GOOGLE_CLIENT_ID: '117548045551-4gh98h2i28obgaeit1ptf6l339srnp0i.apps.googleusercontent.com'
 
 };
